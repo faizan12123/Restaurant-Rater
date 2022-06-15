3c06b54b3df60d69bcf57d5791af5ac73a5bf456
Faizan <faizan121234@gmail.com>
2022-06-14 20:39:33 -0700

    fixing axios API

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -8,8 +8,11 @@ import axios from "axios"
 
 
 //if we are in production baseURL = /api/v1/restaurants else baseURL =  "http://localhost:3001/api/v1/restaurants"
-const baseURL = process.env.NODE_ENV === 'production' ? "api/v1/restaurants" : 'http://localhost:3001/api/v1/restaurants'
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1/restaurants"
+    : "http://localhost:3001/api/v1/restaurants";
 
 export default axios.create({
     baseURL: baseURL,
